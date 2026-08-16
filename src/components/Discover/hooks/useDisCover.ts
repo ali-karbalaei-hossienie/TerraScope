557becb49a90943531bf6bcb8edbc909fa97b654
ali-karbalaei-hossienie <mp.karbalaei@gmail.com>
2026-08-16 23:51:13 +0330

    feat: add cleanup logic in useDisCover for map resources; refactor imports for better organization

--- src/components/Discover/hooks/useDisCover.ts
+++ src/components/Discover/hooks/useDisCover.ts
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMap } from "react-map-gl/mapbox";
 import { useDispatch, useSelector } from "react-redux";
-import type { useDisCoverReturn } from "../types";
-import { setIsSplitMode } from "../../../features/multiMapLayers/multiMapLayersSlice";
-import { generateSourceIds } from "../utils";
-import { addSourceAndLayer, removeMapResources } from "../../utils";
-import type { CardItemType, StringOrNumber } from "../../Card/types";
 import type { RootState } from "../../../app/store";
+import { setIsSplitMode } from "../../../features/multiMapLayers/multiMapLayersSlice";
+import type { CardItemType, StringOrNumber } from "../../Card/types";
+import { addSourceAndLayer, removeMapResources } from "../../utils";
+import type { useDisCoverReturn } from "../types";
+import { generateSourceIds } from "../utils";
 
 export const useDisCover = (): useDisCoverReturn => {
   const { map } = useMap();
@@ -20,6 +20,27 @@ export const useDisCover = (): useDisCoverReturn => {
 
   const [activeCard, setActiveCard] = useState<StringOrNumber[]>([]);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      if (!mapbox) return;
+      const style = mapbox.getStyle();
+
+      if (!style) return;
+
+      style.layers?.forEach((layer) => {
+        if (layer.id.includes("discover")) {
+          if (mapbox.getLayer(layer.id)) mapbox.removeLayer(layer.id);
+        }
+      });
+
+      Object.keys(style.sources).forEach((sourceId) => {
+        if (sourceId.includes("discover")) {
+          if (mapbox.getSource(sourceId)) mapbox.removeSource(sourceId);
+        }
+      });
+    };
+  }, [mapbox]);
 
   const handleImageOnMap = (data: CardItemType) => {
     const { borderSourceId, imageSourceId } = generateSourceIds(data);
